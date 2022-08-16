341235ab27fc888e7f9c760757f43ce2e377c955
shmookoff <shmookoff@gmail.com>
2022-08-17 01:07:47 +0500

    add InsertInlineImage to init

--- tygle_docs/types/requests/__init__.py
+++ tygle_docs/types/requests/__init__.py
@@ -1,5 +1,6 @@
 from .batch_update_body import BatchUpdateBody, WriteControl  # isort:skip
 from .delete_content_range_request import DeleteContentRange
+from .insert_inline_image_request import InsertInlineImage
 from .insert_table_request import InsertTable
 from .replace_all_text_request import ReplaceAllText
 from .update_table_cell_style_request import UpdateTableCellStyle
