075b3d8990055ba8dfc8674beb92a8b1a817199a
shmookoff <shmookoff@gmail.com>
2022-08-16 08:07:28 +0500

    segment_id can be empty

--- tygle_docs/types/resources/named_ranges/named_range/range/range.py
+++ tygle_docs/types/resources/named_ranges/named_range/range/range.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
 class Range(BaseModel):
-    segment_id: str = Field(alias="segmentId")
+    segment_id: Optional[str] = Field(None, alias="segmentId")
     start_index: int = Field(alias="startIndex")
     end_index: int = Field(alias="endIndex")
