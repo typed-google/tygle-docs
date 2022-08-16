9f16ef5adca78ac4ac556cb64f9c55a084b3101c
shmookoff <shmookoff@gmail.com>
2022-08-17 02:17:42 +0500

    add substringmatchcriteria to init

--- tygle_docs/types/requests/__init__.py
+++ tygle_docs/types/requests/__init__.py
@@ -3,5 +3,5 @@ from .delete_content_range_request import DeleteContentRange
 from .insert_inline_image_request import InsertInlineImage
 from .insert_table_request import InsertTable
 from .models import *
-from .replace_all_text_request import ReplaceAllText
+from .replace_all_text_request import ReplaceAllText, SubstringMatchCriteria
 from .update_table_cell_style_request import UpdateTableCellStyle
